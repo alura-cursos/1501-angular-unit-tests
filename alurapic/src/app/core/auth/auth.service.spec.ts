3c3ca79946ab1acb1d51da323b12a4b115f03302
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-25 00:35:37 -0300

    Aula 3.4

--- alurapic/src/app/core/auth/auth.service.spec.ts
+++ alurapic/src/app/core/auth/auth.service.spec.ts
@@ -3,7 +3,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController
 } from "@angular/common/http/testing";
-import { TestBed } from "@angular/core/testing";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { UserService } from "../user/user.service";
 
 describe("O serviço AuthService", () => {
@@ -25,18 +25,18 @@ describe("O serviço AuthService", () => {
     expect(service).toBeTruthy();
   });
 
-  it("deve autenticar o usuario", () => {
+  it("deve autenticar o usuario", fakeAsync(() => {
     const fakeBody = {
       id: 1,
       nome: "alvaro",
       email: "alvaro@alura.com"
     };
 
-    spyOn(userService, "setToken").and.returnValue(null);
+    const spy = spyOn(userService, "setToken").and.returnValue(null);
 
     service.authenticate("alvaro", "1234").subscribe(response => {
       expect(response.body).toEqual(fakeBody);
-      expect(response.headers.get("x-acess-token")).toBe("tokenTest");
+      expect(spy).toHaveBeenCalledWith("tokenTest");
     });
 
     const request = httpMock.expectOne(req => {
@@ -44,7 +44,9 @@ describe("O serviço AuthService", () => {
     });
 
     request.flush(fakeBody, {
-      headers: { "x-acess-token": "tokenTest" }
+      headers: { "x-access-token": "tokenTest" }
     });
-  });
+
+    tick();
+  }));
 });
