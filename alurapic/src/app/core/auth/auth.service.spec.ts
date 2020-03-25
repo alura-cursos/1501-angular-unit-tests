11da49bb890542e05f135b00868c1d391803ed9d
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-25 00:32:26 -0300

    Aula 3.2

--- alurapic/src/app/core/auth/auth.service.spec.ts
+++ alurapic/src/app/core/auth/auth.service.spec.ts
@@ -1,20 +1,37 @@
 import { AuthService } from "./auth.service";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { UserService } from "../user/user.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 
 describe("O serviço AuthService", () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
       providers: [AuthService]
     });
     service = TestBed.get(AuthService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it("deve ser instanciado", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("deve autenticar o usuario", () => {
+    const fakeBody = {
+      id: 1,
+      nome: "alvaro",
+      email: "alvaro@alura.com"
+    };
+
+    service.authenticate("alvaro", "1234").subscribe(response => {
+      expect(response.body).toEqual(fakeBody);
+      expect(response.headers.get("x-acess-token")).toBe("tokenTest");
+    });
   });
 });
