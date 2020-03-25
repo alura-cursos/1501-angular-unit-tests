f53b05af35262e73f57c5df924ba260ddefd2168
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-25 00:28:24 -0300

    Aula 2.4

--- alurapic/src/app/core/user/user.service.spec.ts
+++ alurapic/src/app/core/user/user.service.spec.ts
@@ -1,12 +1,15 @@
 import { UserService } from "./user.service";
 import { TokenService } from "../token/token.service";
+import { TestBed } from "@angular/core/testing";
 
 describe("O serviço UserService", () => {
   let service: UserService;
 
   beforeEach(() => {
-    const tokenService = new TokenService();
-    service = new UserService(tokenService);
+    TestBed.configureTestingModule({
+      providers: [UserService]
+    });
+    service = TestBed.get(UserService);
   });
 
   it("deve ser instanciado", () => {
@@ -19,5 +22,17 @@ describe("O serviço UserService", () => {
     service.setToken(token);
     expect(service.isLogged()).toBeTruthy();
     expect(service.getUserName()).toBe("flavio");
+    service.getUser().subscribe(user => {
+      expect(user.name).toBe("flavio");
+    });
+  });
+
+  it("deve limpar as informações no logout", () => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImZsYXZpbyIsImVtYWlsIjoiZmxhdmlvQGFsdXJhcGljLmNvbS5iciIsImlhdCI6MTU4MDk0NDcxOCwiZXhwIjoxNTgxMDMxMTE4fQ.irVPxUq3o6ntq08_XHs9tuDQbuOrL4Z6cCkonc1exqc";
+    service.setToken(token);
+    service.logout();
+    expect(service.isLogged()).toBeFalsy();
+    expect(service.getUserName()).toBe("");
   });
 });
